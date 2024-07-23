e970e5f16738c4ec5a4a0154c5772570d9af9f74
Ladislav Prix <elix78963@gmail.com>
2024-07-23 10:03:06 +0200

    feat: application - print usage on error, read xml from strdin

--- App/Main.hs
+++ App/Main.hs
@@ -1,32 +1,57 @@
 module App.Main where
 
+import Control.Monad
 import Lib.Parser
 import Lib.Serialize
 import Lib.XmlParser
 import Lib.XqParser
 import Lib.XqRunner
+import System.Directory.Internal.Prelude
+import System.Exit
 
-parseFile :: FilePath -> Parser a -> IO (Maybe a)
-parseFile fileName parser = do
-  input <- readFile fileName
-  pure (snd <$> runParser parser input)
+usage :: String
+usage =
+  "Usage: xq <xQuery> <xmlFilePath>\n\
+  \Usage: xq <xQuery> {xmlData from stdin}\n\
+  \Example: xq \"//book/*\" \"./bookstore.xml\"\n\
+  \Example: cat \"./bookstore.xml\" | Examples: xq \"//book/*\""
 
-queryXml :: String -> String -> Maybe [XmlValue]
-queryXml q x = do
-  (_, query) <- runParser xqParser q
-  (_, xml) <- runParser xmlParser x
+exit :: String -> IO ()
+exit err = do
+  putStrLn err
+  putStrLn usage
+  exitFailure
 
-  pure $ runXq query xml
+maybeToEither :: String -> Maybe a -> Either String a
+maybeToEither s Nothing = Left s
+maybeToEither _ (Just val) = Right val
 
-queryFile :: String -> FilePath -> IO (Maybe [XmlValue])
-queryFile query fileName = do
-  fileContent <- readFile fileName
-  pure (queryXml query fileContent)
+inputIsEmpty :: (String, a) -> Maybe (String, a)
+inputIsEmpty res = if null (fst res) then Just res else Nothing
+
+queryXml :: (String, String) -> Either String [XmlValue]
+queryXml (x, q) = do
+  (_, xml) <- maybeToEither "Unable to parse the XML." (runParser xmlParser x >>= inputIsEmpty)
+  (_, xq) <- maybeToEither "Unable to parse the XQuery." (runParser xqParser q >>= inputIsEmpty)
+  Right $ runXq xq xml
+
+getInputs :: IO (Either String (String, String))
+getInputs = do
+  args <- getArgs
+  let argLen = length args
+
+  if argLen /= 2 && argLen /= 1
+    then pure $ Left "Invalid number of arguments"
+    else do
+      xmlString <-
+        if argLen == 2
+          then readFile (args !! 1)
+          else getContents
+      pure $ Right (xmlString, head args)
 
 main :: IO ()
 main = do
-  res <- queryFile "/bookstore/*//book" "./.output/basic.xml"
-  case serialize <$> res of
-    Just a -> putStrLn a
-    Nothing -> error "Err."
-  pure ()
+  inputs <- getInputs
+  case inputs >>= queryXml of
+    Right value -> putStrLn (serialize value)
+    Left err -> exit err
