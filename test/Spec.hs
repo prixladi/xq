5f569f93239ca1c08220f25636a0028ad18b2545
prixladi <elix78963@gmail.com>
2024-08-21 18:54:38 +0200

    feat: Enhanced test tooling and execution

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,6 +1,8 @@
 module Main (main) where
 
+import Data.Either
 import Helpers
+import System.Exit
 import XmlParserTest
 import XqParserTest
 import XqRunnerTest
@@ -11,11 +13,24 @@ printResult (name, Right ()) = putStrLn $ "  " ++ name ++ " - Success"
 
 main :: IO ()
 main = do
+  let xmlParserResult = runAll XmlParserTestModule
   putStrLn "xmlParserTests:"
-  mapM_ printResult $ runAll XmlParserTestModule
+  mapM_ printResult xmlParserResult
 
+  let xqParserResult = runAll XqParserTestModule
   putStrLn "xqParserTests:"
-  mapM_ printResult $ runAll XqParserTestModule
+  mapM_ printResult xqParserResult
 
+  let xqRunnerResult = runAll XqRunnerTestModule
   putStrLn "xqRunnerTests:"
-  mapM_ printResult $ runAll XqRunnerTestModule
+  mapM_ printResult xqRunnerResult
+
+  let allResults = snd <$> concat [xmlParserResult, xqParserResult, xqRunnerResult]
+  let passedCount = length $ filter isRight allResults
+  let failedCount = length $ filter isLeft allResults
+
+  putStrLn $ "\n\nPassed: " ++ show passedCount ++ " Failed: " ++ show failedCount
+
+  if failedCount == 0
+    then exitSuccess
+    else exitWith (ExitFailure 1)
